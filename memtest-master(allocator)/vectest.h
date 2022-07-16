7f1ea9c1b5f3d2e0bd380688aeb7136d6010cc30
powcoder代写 <powcoder@163.com>
2022-07-16 14:45:07 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ memtest-master(allocator)/vectest.h
@@ -0,0 +1,19 @@
+//
+// Created by mark on 2/6/20.
+//
+
+#ifndef MEMTEST_VECTEST_H
+#define MEMTEST_VECTEST_H
+#include <iostream>
+#include <new>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+
+void somefunction();
+
+
+
+
+#endif //MEMTEST_VECTEST_H
